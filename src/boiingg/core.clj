1fbacfc774b5d01f15255da3e1f5ce6f4506b864
Sam Aaron <samaaron@gmail.com>
2010-09-22 14:56:27 +0100

    initial commit

---
+++ src/boiingg/core.clj
@@ -0,0 +1,1 @@
+(ns boiingg.core)
---
+++ test/boiingg/test/core.clj
@@ -0,0 +1,6 @@
+(ns boiingg.test.core
+  (:use [boiingg.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
